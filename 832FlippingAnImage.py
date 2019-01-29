9fcc6eeddfdc51400c7d42074cbd9c09ae37c982
Jay <flamesunrises@gmail.com>
2019-01-29 14:34:23 +0800

    Track 1 files into repository.
    - modified 832FlippingAnImage.py
    
    Auto commit by GitBook Editor

---
+++ 832FlippingAnImage.py
@@ -0,0 +1,11 @@
+class Solution:
+    def flipAndInvertImage(self, A):
+        """
+        :type A: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        
+        
+if __name__ == '__main__':
+    solution = Solution()
+    print (solution.flipAndInvertImage([3,1,2,4]))
