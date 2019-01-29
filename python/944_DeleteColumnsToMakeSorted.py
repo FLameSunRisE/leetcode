3a520f8910cd0df423cad98a72c37ef2b184d1b9
Jay <flamesunrises@gmail.com>
2019-01-29 15:43:10 +0800

    Track 1 files into repository.
    - untracked python/944_DeleteColumnsToMakeSorted.py
    
    Auto commit by GitBook Editor

---
+++ python/944_DeleteColumnsToMakeSorted.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minDeletionSize(self, A):
+        """
+        :type A: List[str]
+        :rtype: int
+        """
+
+if __name__ == '__main__':
+    solution = Solution()
+    print (solution.minDeletionSize(["cba","daf","ghi"]))
+    print (solution.minDeletionSize(["a","b"]))
+    print (solution.minDeletionSize(["zyx","wvu","tsr"]))
