e1613e5c3ae8ab217850fde5f5302ffbdc375360
Jay <flamesunrises@gmail.com>
2019-01-29 15:06:49 +0800

    Track 1 files into repository.
    - modified 657_RobotReturnToOrigin.py
    
    Auto commit by GitBook Editor

---
+++ 657_RobotReturnToOrigin.py
@@ -0,0 +1,12 @@
+class Solution:
+    def judgeCircle(self, moves):
+        """
+        :type moves: str
+        :rtype: bool
+        """
+        
+        
+if __name__ == '__main__':
+    solution = Solution()
+    print (solution.judgeCircle("UD"))
+    print (solution.judgeCircle("LL"))
