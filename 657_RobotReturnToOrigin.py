ce9e562781a8f0aae14d03f4f17fb3be815b918b
Jay <flamesunrises@gmail.com>
2019-01-29 15:09:18 +0800

    Track 1 files into repository.
    - modified 657_RobotReturnToOrigin.py
    
    Auto commit by GitBook Editor

--- 657_RobotReturnToOrigin.py
+++ 657_RobotReturnToOrigin.py
@@ -4,7 +4,15 @@ class Solution:
         :type moves: str
         :rtype: bool
         """
-        
+        moveList = list(moves)
+        numU = moves.count("U")
+        numD = moves.count("D")
+        numR = moves.count("R")
+        numL = moves.count("L")
+        if numU==numD and numR==numL:
+            return True
+        else:
+            return False
         
 if __name__ == '__main__':
     solution = Solution()
