7bc91a6afd9b01a687fa3151170c3f6085d225e4
Jay <flamesunrises@gmail.com>
2019-01-29 15:17:10 +0800

    Track 1 files into repository.
    - untracked python/942_DIStringMatch.py
    
    Auto commit by GitBook Editor

---
+++ python/942_DIStringMatch.py
@@ -0,0 +1,13 @@
+class Solution:
+    def diStringMatch(self, S):
+        """
+        :type S: str
+        :rtype: List[int]
+        """
+
+        
+if __name__ == '__main__':
+    solution = Solution()
+    print (solution.diStringMatch("IDID"))
+    print (solution.diStringMatch("III"))
+    print (solution.diStringMatch("DDI"))
