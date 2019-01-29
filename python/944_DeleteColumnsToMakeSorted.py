c3000c1efae376bbcef5860646cae4337ff66cd9
Jay <flamesunrises@gmail.com>
2019-01-29 16:45:25 +0800

    Track 1 files into repository.
    - modified python/944_DeleteColumnsToMakeSorted.py
    
    Auto commit by GitBook Editor

--- python/944_DeleteColumnsToMakeSorted.py
+++ python/944_DeleteColumnsToMakeSorted.py
@@ -4,6 +4,20 @@ class Solution:
         :type A: List[str]
         :rtype: int
         """
+        count = 0
+        for col in zip(*A):
+            if sorted(col) != list(col):
+                count += 1
+        return count
+
+        # for num in range(len(A[0])):
+        #     tmp = []
+        #     for word in A:
+        #         tmp.append(ord(word[num]))
+        #         pass
+        #     if sorted(tmp) != tmp:
+        #         count += 1
+        #     pass
 
 if __name__ == '__main__':
     solution = Solution()
