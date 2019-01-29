8d2dbad7cc39121e931f4305d423cac9aa373ed6
Jay <flamesunrises@gmail.com>
2019-01-29 14:20:51 +0800

    Track 1 files into repository.
    - modified python/905.SortArrayByParity.py
    
    Auto commit by GitBook Editor

--- python/905.SortArrayByParity.py
+++ python/905.SortArrayByParity.py
@@ -4,15 +4,24 @@ class Solution:
         :type A: List[int]
         :rtype: List[int]
         """
-        oddList = []
-        evenList = []
-        for i in A:
-            if i % 2 == 0:
-                evenList.append(i)
-            else:
-                oddList.append(i)
-        evenList.extend(oddList)
-        return evenList
+        # oddList = []
+        # evenList = []
+        # for i in A:
+        #     if i % 2 == 0:
+        #         evenList.append(i)
+        #     else:
+        #         oddList.append(i)
+        # evenList.extend(oddList)
+        # return evenList
+        i, j = 0, len(A) - 1
+        while i < j:
+            if A[i] % 2 > A[j] % 2:
+                A[i], A[j] = A[j], A[i]
+
+            if A[i] % 2 == 0: i += 1
+            if A[j] % 2 == 1: j -= 1
+
+        return A
 
         
 if __name__ == '__main__':
