593e09019d6fde7794534673ef5e8de7ad40a106
Jay <flamesunrises@gmail.com>
2019-01-30 12:28:28 +0800

    Track 1 files into repository.
    - modified python/965_UnivaluedBinaryTree.py
    
    Auto commit by GitBook Editor

---
+++ python/965_UnivaluedBinaryTree.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def isUnivalTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+
+if __name__ == '__main__':
+    solution = Solution()
+    print (solution.isUnivalTree([1,1,1,1,1,null,1]))
+    print (solution.isUnivalTree([2,2,2,5,2]))
