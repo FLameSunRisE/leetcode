1ca7a319c5fc8b4bc86747a1f24a618cfac23cb1
Jay <flamesunrises@gmail.com>
2019-02-04 14:41:30 +0800

    Track 2 files into repository.
    - modified python/965_UnivaluedBinaryTree.py
    - modified python/617_MergeTwoBinaryTrees.py
    
    Auto commit by GitBook Editor

--- python/965_UnivaluedBinaryTree.py
+++ python/965_UnivaluedBinaryTree.py
@@ -1,9 +1,42 @@
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+def stringToTreeNode(input):
+    input = input.strip()
+    input = input[1:-1]
+    if not input:
+        return None
+
+    inputValues = [s.strip() for s in input.split(',')]
+    root = TreeNode(int(inputValues[0]))
+    nodeQueue = [root]
+    front = 0
+    index = 1
+    while index < len(inputValues):
+        node = nodeQueue[front]
+        front = front + 1
+
+        item = inputValues[index]
+        index = index + 1
+        if item != "null":
+            leftNumber = int(item)
+            node.left = TreeNode(leftNumber)
+            nodeQueue.append(node.left)
+
+        if index >= len(inputValues):
+            break
+
+        item = inputValues[index]
+        index = index + 1
+        if item != "null":
+            rightNumber = int(item)
+            node.right = TreeNode(rightNumber)
+            nodeQueue.append(node.right)
+    return root
 
 class Solution:
     def isUnivalTree(self, root):
@@ -11,19 +44,24 @@ class Solution:
         :type root: TreeNode
         :rtype: bool
         """
-        vals = []
-
+        ansList = []
         def dfs(node):
             if node:
-                vals.append(node.val)
+                ansList.append(node.val)
                 dfs(node.left)
                 dfs(node.right)
 
         dfs(root)
-        return len(set(vals)) == 1
+        ansSet = set(ansList)
+        if len(ansSet)==1:
+            return True
+        return False
 
 
 if __name__ == '__main__':
     solution = Solution()
-    print (solution.isUnivalTree([1,1,1,1,1,null,1]))
-    print (solution.isUnivalTree([2,2,2,5,2]))
+    tree = stringToTreeNode('[1,1,1,1,1,null,1]')
+    print(solution.isUnivalTree(tree))
+    tree = stringToTreeNode('[2,2,2,5,2]')
+    print(solution.isUnivalTree(tree))
+
