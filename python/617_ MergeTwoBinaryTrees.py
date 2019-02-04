bc670a12c9572bcd510148993f60a69fe917f9c1
Jay <flamesunrises@gmail.com>
2019-02-04 09:50:46 +0800

    Track 1 files into repository.
    - modified python/617_ MergeTwoBinaryTrees.py
    
    Auto commit by GitBook Editor

--- python/617_ MergeTwoBinaryTrees.py
+++ python/617_ MergeTwoBinaryTrees.py
@@ -1,9 +1,62 @@
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+def stringToTreeNode(input):
+    input = input.strip()
+    input = input[1:-1]
+    if not input:
+        return None
+
+    inputValues = [s.strip() for s in input.split(',')]
+    root = TreeNode(int(inputValues[0]))
+    nodeQueue = [root]
+    front = 0
+    index = 1
+    while index < len(inputValues):
+        node = nodeQueue[front]
+        front = front + 1
+
+        item = inputValues[index]
+        index = index + 1
+        if item != "null":
+            leftNumber = int(item)
+            node.left = TreeNode(leftNumber)
+            nodeQueue.append(node.left)
+
+        if index >= len(inputValues):
+            break
+
+        item = inputValues[index]
+        index = index + 1
+        if item != "null":
+            rightNumber = int(item)
+            node.right = TreeNode(rightNumber)
+            nodeQueue.append(node.right)
+    return root
+
+def treeNodeToString(root):
+    if not root:
+        return "[]"
+    output = ""
+    queue = [root]
+    current = 0
+    while current != len(queue):
+        node = queue[current]
+        current = current + 1
+
+        if not node:
+            output += "null, "
+            continue
+
+        output += str(node.val) + ", "
+        queue.append(node.left)
+        queue.append(node.right)
+    return "[" + output[:-2] + "]"
 
 class Solution:
     def mergeTrees(self, t1, t2):
@@ -12,7 +65,20 @@ class Solution:
         :type t2: TreeNode
         :rtype: TreeNode
         """
+        if t1 is None:
+            return t2
+        if t2 is None:
+            return t1
+        t1.val += t2.val
+        t1.right = self.mergeTrees(t1.right,t2.right)
+        t1.left = self.mergeTrees(t1.left,t2.left)
+        return t1
 
 if __name__ == '__main__':
     solution = Solution()
-    print (solution.mergeTrees(1,4))
+    t1 = stringToTreeNode('[1,3,2,5]')
+    t2 = stringToTreeNode('[2,1,3,null,4,null,7]')
+
+    ret = solution.mergeTrees(t1,t2)
+    out = treeNodeToString(ret)
+    print(out)
