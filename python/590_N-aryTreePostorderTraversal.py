dea83401b709f011bb4b9abbdeab66708920f33e
Jay <flamesunrises@gmail.com>
2019-02-07 17:12:00 +0800

    Track 1 files into repository.
    - modified python/590_N-aryTreePostorderTraversal.py
    
    Auto commit by GitBook Editor

---
+++ python/590_N-aryTreePostorderTraversal.py
@@ -0,0 +1,16 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, children):
+        self.val = val
+        self.children = children
+"""
+class Solution:
+    def postorder(self, root: 'Node') -> 'List[int]':
+        ans = []
+        if not root:
+            return ans
+        for child in root.children:
+            ans.extend(self.postorder(child))
+        ans.append(root.val)
+        return ans
