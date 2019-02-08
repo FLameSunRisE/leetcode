aa60c89d14593be767eb182bd61981b136848d6d
Jay <flamesunrises@gmail.com>
2019-02-08 15:32:19 +0800

    Track 1 files into repository.
    - modified python/883_ProjectionAreaOf3DShapes.py
    
    Auto commit by GitBook Editor

--- python/883_ProjectionAreaOf3DShapes.py
+++ python/883_ProjectionAreaOf3DShapes.py
@@ -1,4 +1,25 @@
 class Solution:
     def projectionArea(self, grid: 'List[List[int]]') -> 'int':
-        
-        return 0
+        xy,yz,xz = 0,0,0
+        N = len(grid)
+        for i in range(N):
+            tmpYZ,tmpXZ = 0,0
+            for j in range(N):
+                if grid[i][j]!=0:
+                    xy += 1
+                tmpYZ = max(tmpYZ,grid[j][i])
+                tmpXZ = max(tmpXZ,grid[i][j])
+            yz += tmpYZ
+            xz += tmpXZ
+        # print('xy : ' +str(xy))
+        # print('yz : ' +str(yz))
+        # print('xz : ' +str(xz))
+        return xy+yz+xz
+
+if __name__ == '__main__':
+    solution = Solution()
+    print (solution.projectionArea([[2]])) #5
+    print (solution.projectionArea([[1,2],[3,4]])) #17
+    print (solution.projectionArea([[1,0],[0,2]])) #8
+    print (solution.projectionArea([[1,1,1],[1,0,1],[1,1,1]])) #14
+    print (solution.projectionArea([[2,2,2],[2,1,2],[2,2,2]])) #21
