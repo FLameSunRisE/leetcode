cabdddcfb7e3b6902bc7e7f8be246b4ac5214ff2
Jay <flamesunrises@gmail.com>
2019-02-08 17:13:01 +0800

    Track 2 files into repository.
    - modified python/559_MaximumDepthOfN-aryTree.py
    - modified python/590_N-aryTreePostorderTraversal.py
    
    Auto commit by GitBook Editor

---
+++ python/559_MaximumDepthOfN-aryTree.py
@@ -0,0 +1,18 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, children):
+        self.val = val
+        self.children = children
+"""
+class Solution:
+    def maxDepth(self, root: 'Node') -> 'int':
+        
+
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    print(solution.smallestRangeI([1], 0))  # 0
+    print(solution.smallestRangeI([0, 10], 2))  # 6
+    print(solution.smallestRangeI([1, 3, 6], 3))  # 0
