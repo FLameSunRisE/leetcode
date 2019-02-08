e24f91850f6ad933cb65769e806f0ebf1ec677cb
Jay <flamesunrises@gmail.com>
2019-02-08 21:16:34 +0800

    Track 1 files into repository.
    - modified python/867_TransposeMatrix.py
    
    Auto commit by GitBook Editor

--- python/867_TransposeMatrix.py
+++ python/867_TransposeMatrix.py
@@ -1,19 +1,12 @@
 class Solution:
     def transpose(self, A: 'List[List[int]]') -> 'List[List[int]]':
-        ansList = []
-        for i in range(len(A)+1):
-            ansList.append([])
-        print('ansList')
-        print(ansList)
-        for i in range(len(A)):
-            print ('i:'+str(i))
-            print (A[i])
-            for x in range(len(A[i])):
-                print ('--'+str(x))
-                print ('--'+str(x)+':'+str(A[i][x]))
-                ansList[x].append(A[i][x])
-        print (ansList)
-        return ansList
+        ans = []
+        for x in range(len(A[0])):
+            tmp = []
+            for y in range(len(A)):
+                tmp.append(A[y][x])
+            ans.append(tmp)
+        return ans
 
 if __name__ == '__main__':
     solution = Solution()
