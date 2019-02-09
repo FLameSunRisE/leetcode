11a11648b955f5c77f8c55cdb54e9521b78855f6
Jay <flamesunrises@gmail.com>
2019-02-09 16:34:17 +0800

    Track 1 files into repository.
    - untracked python/344_ReverseString.py
    
    Auto commit by GitBook Editor

---
+++ python/344_ReverseString.py
@@ -0,0 +1,23 @@
+class Solution:
+    def reverseString(self, s: 'List[str]') -> 'None':
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        min = 0
+        max = len(s)-1
+        print(''.join(reversed(s)))
+        while min != max and min < max:
+            s[min], s[max] = s[max], s[min]
+            min += 1
+            max -= 1
+        
+        print(s)
+        return 
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    print(solution.reverseString(["h", "e", "l", "l", "o"]))
+    # Output:  ["o","l","l","e","h"]
+    print(solution.reverseString(["H","a","n","n","a","h"]))
+    # Output:  ["h","a","n","n","a","H"]
