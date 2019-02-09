f64f6c0d60194a043fb6b34090947b2ba024f48b
Jay <flamesunrises@gmail.com>
2019-02-09 16:12:43 +0800

    Track 1 files into repository.
    - modified python/557_ReverseWordsInAStringIII.py
    
    Auto commit by GitBook Editor

--- python/557_ReverseWordsInAStringIII.py
+++ python/557_ReverseWordsInAStringIII.py
@@ -1,16 +1,12 @@
 class Solution:
-    def reverseWords(self, s):
-        """
-        :type s: str
-        :rtype: str
-        """
-        strList = s.split(' ')
-        res = ''
-        resList = []
-        for s in range(len(strList)):
-            splitStr = strList[s][::-1]
-            if(s == len(strList)-1):
-                res += splitStr
-            else:
-                res += splitStr+' '            
-        return res
+    def reverseWords(self, s: 'str') -> 'str':
+        splList = s.split(' ')
+        for i in range(len(splList)):
+            splList[i] = splList[i][::-1]
+        print(splList)
+        return ' '.join(splList)
+
+if __name__ == '__main__':
+    solution = Solution()
+    print(solution.reverseWords("Let's take LeetCode contest"))
+    # Output: "s'teL ekat edoCteeL tsetnoc"
