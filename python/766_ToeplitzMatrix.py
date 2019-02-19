4941ed0718c22fd98a23a55967e449c55587ef6c
Jay <flamesunrises@gmail.com>
2019-02-19 15:40:28 +0800

    Track 1 files into repository.
    - modified python/766_ToeplitzMatrix.py
    
    Auto commit by GitBook Editor

--- python/766_ToeplitzMatrix.py
+++ python/766_ToeplitzMatrix.py
@@ -1,13 +1,9 @@
 class Solution:
-    def checkSlot(self,matrix,)
     def isToeplitzMatrix(self, matrix: 'List[List[int]]') -> 'bool':
-        n = len(matrix)
-        m = len(matrix[0])
-        x , y = 0 , 0
-        
-
-
-
+        for i in range(len(matrix) - 1):
+            for j in range(len(matrix[0]) - 1):
+                if matrix[i][j] != matrix[i + 1][j + 1]:
+                    return False
         return True
 
 
