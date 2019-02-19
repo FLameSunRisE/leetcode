eaeb1cba214ac4e1e38539f571bee0c75617e983
Jay <flamesunrises@gmail.com>
2019-02-19 15:18:44 +0800

    Track 1 files into repository.
    - untracked python/766_ToeplitzMatrix.py
    
    Auto commit by GitBook Editor

---
+++ python/766_ToeplitzMatrix.py
@@ -0,0 +1,21 @@
+class Solution:
+    def checkSlot(self,matrix,)
+    def isToeplitzMatrix(self, matrix: 'List[List[int]]') -> 'bool':
+        n = len(matrix)
+        m = len(matrix[0])
+        x , y = 0 , 0
+        
+
+
+
+        return True
+
+
+def main():
+    input = [[1,2,3,4],[5,1,2,3],[9,5,1,2]]
+    # Output: ["Alaska", "Dad"]
+    print(Solution().isToeplitzMatrix(input))
+
+
+if __name__ == '__main__':
+    main()
