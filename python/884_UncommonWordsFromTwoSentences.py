792d2cf2f807c20a7ac9649a39c02959303fb2f0
Jay <flamesunrises@gmail.com>
2019-02-19 18:47:05 +0800

    Track 1 files into repository.
    - untracked python/884_UncommonWordsFromTwoSentences.py
    
    Auto commit by GitBook Editor

---
+++ python/884_UncommonWordsFromTwoSentences.py
@@ -0,0 +1,13 @@
+class Solution:
+    def uncommonFromSentences(self, A: 'str', B: 'str') -> 'List[str]':
+        
+        return A
+
+
+def main():
+    input = [[1,2,3,4],[5,1,2,3],[9,5,1,2]]
+    # Output: ["Alaska", "Dad"]
+    print(Solution().isToeplitzMatrix(input))
+
+if __name__ == '__main__':
+    main()
