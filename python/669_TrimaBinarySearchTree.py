25a75ce0a599b38046826da9d4155ceb6f9a6deb
Jay <flamesunrises@gmail.com>
2019-02-19 20:47:08 +0800

    Track 1 files into repository.
    - untracked python/669_TrimaBinarySearchTree.py
    
    Auto commit by GitBook Editor

---
+++ python/669_TrimaBinarySearchTree.py
@@ -0,0 +1,84 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+def stringToTreeNode(input):
+    input = input.strip()
+    input = input[1:-1]
+    if not input:
+        return None
+
+    inputValues = [s.strip() for s in input.split(',')]
+    root = TreeNode(int(inputValues[0]))
+    nodeQueue = [root]
+    front = 0
+    index = 1
+    while index < len(inputValues):
+        node = nodeQueue[front]
+        front = front + 1
+
+        item = inputValues[index]
+        index = index + 1
+        if item != "null":
+            leftNumber = int(item)
+            node.left = TreeNode(leftNumber)
+            nodeQueue.append(node.left)
+
+        if index >= len(inputValues):
+            break
+
+        item = inputValues[index]
+        index = index + 1
+        if item != "null":
+            rightNumber = int(item)
+            node.right = TreeNode(rightNumber)
+            nodeQueue.append(node.right)
+    return root
+
+
+def treeNodeToString(root):
+    if not root:
+        return "[]"
+    output = ""
+    queue = [root]
+    current = 0
+    while current != len(queue):
+        node = queue[current]
+        current = current + 1
+
+        if not node:
+            output += "null, "
+            continue
+
+        output += str(node.val) + ", "
+        queue.append(node.left)
+        queue.append(node.right)
+    return "[" + output[:-2] + "]"
+
+
+class Solution:
+    def trimBST(self, root: 'TreeNode', L: 'int', R: 'int') -> 'TreeNode':
+        if not root:
+            return root
+        if root.val < L:
+            return self.trimBST(root.right, L, R)
+        if root.val > R:
+            return self.trimBST(root.left, L, R)
+        root.left = self.trimBST(root.left, L, R)
+        root.right = self.trimBST(root.right, L, R)
+        return root
+
+def main():
+    root = stringToTreeNode("[1,0,2]")
+    L = 1
+    R = 2
+    ret = Solution().trimBST(root, L, R)
+    out = treeNodeToString(ret)
+    print(out)
+
+if __name__ == '__main__':
+    main()
