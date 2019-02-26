023394f7ce2e1eeed231abdbf33edb880470cfb1
Jay <flamesunrises@gmail.com>
2019-02-26 15:43:35 +0800

    Track 1 files into repository.
    - modified python/521_LongestUncommonSubsequenceI.py
    
    Auto commit by GitBook Editor

--- python/521_LongestUncommonSubsequenceI.py
+++ python/521_LongestUncommonSubsequenceI.py
@@ -2,8 +2,20 @@
 
 class Solution:
     def findLUSlength(self, a: str, b: str) -> int:
-        return 0
+        if a == b:
+            return False
+        return max(len(a), len(b))
 
 
+
+def main():
+    print(Solution().findLUSlength("aba","cdc"))
+    # Output: 3
+    # Explanation: The longest uncommon subsequence is "aba" (or "cdc"), 
+    # because "aba" is a subsequence of "aba", 
+    # but not a subsequence of any other strings in the group of two strings. 
+    
+if __name__ == '__main__':
+    main()
 
         
