7332daa25e6293564a434a9503cee5a3e7b8a141
FLameSunRisE <flamesunrises@gmail.com>
2022-06-22 23:55:11 +0800

    daily: 69 1/2

---
+++ python/69_Sqrt(x).py
@@ -0,0 +1,25 @@
+from typing import List
+
+
+class Solution:
+    def mySqrt(self, x: 'int') -> 'int':
+
+
+def main():
+    # Example:1
+    x = 4
+    print(Solution().mySqrt(x))
+    # Input: x = 4
+    # Output: 2
+    print('------------------------------')
+    # Example:2
+    x = 8
+    print(Solution().mySqrt(x))
+    # Input: x = 8
+    # Output: 2
+    # Explanation: The square root of 8 is 2.82842..., and since the decimal part is truncated, 2 is returned.
+    print('------------------------------')
+
+
+if __name__ == '__main__':
+    main()
