e6b21e0820b600e1ac0d5bab561b57e3a52ed293
FLameSunRisE <flamesunrises@gmail.com>
2023-03-30 23:58:05 +0800

    daily: not finish

---
+++ java/leetcode/src/main/hashmap/FindCommonCharacters1002.java
@@ -0,0 +1,16 @@
+package main.hashmap;
+
+/**
+ * FindCommonCharacters1002.
+ *
+ * @author jayyeh
+ * @version 1.0.0
+ * @date 2023/03/30
+ */
+public class FindCommonCharacters1002 {
+    public Object[] commonChars(String[] A) {
+
+        return null;
+    }
+}
+}
---
+++ java/leetcode/src/main/hashmap/FindCommonCharacters1002Test.java
@@ -0,0 +1,30 @@
+package main.hashmap;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+class FindCommonCharacters1002Test {
+
+    private FindCommonCharacters1002 solution;
+
+    @BeforeEach
+    void setUp() {
+        this.solution = new FindCommonCharacters1002();
+    }
+
+    @Test
+    public void test1() {
+        String[] words = {"bella", "label", "roller"};
+        String[] expected = {"e", "l", "l"};
+        assertArrayEquals(expected, solution.commonChars(words));
+    }
+
+    @Test
+    public void test2() {
+        String[] words = {"cool", "lock", "cook"};
+        String[] expected = {"c", "o"};
+        assertArrayEquals(expected, solution.commonChars(words));
+    }
+}
